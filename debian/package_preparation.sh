6748935c64c9141de5e36e22b2dfae6dfcfc812e
Hyoung Joo Ahn <hello.ahn@samsung.com>
2020-02-12 15:15:19 +0900

    [Initial] set TF&TFLite v1.15
    Signed-off-by: Hyoung Joo Ahn <hello.ahn@samsung.com>

---
+++ debian/package_preparation.sh
@@ -0,0 +1,15 @@
+cat $packages_path/0e906ebc527eab1cdbf7adabff5b474da9562e9f.tar.gz.* > $packages_path/0e906ebc527eab1cdbf7adabff5b474da9562e9f.tar.gz
+cat $packages_path/bazel_0.25.2.tar.gz.* > $packages_path/bazel_0.25.2.tar.gz
+cat $packages_path/v6.7.2.tar.gz.* > $packages_path/v6.7.2.tar.gz
+cat $packages_path/v3.4.0.tar.gz.* > $packages_path/v3.4.0.tar.gz
+cat $packages_path/mobilenet_v1_1.0_224.tgz.* > $packages_path/mobilenet_v1_1.0_224.tgz
+cat $packages_path/ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18.tar.gz.* > $packages_path/ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18.tar.gz
+cat $packages_path/mklml_lnx_2019.0.5.20190502.tgz.* > $packages_path/mklml_lnx_2019.0.5.20190502.tgz
+
+rm $packages_path/0e906ebc527eab1cdbf7adabff5b474da9562e9f.tar.gz.*
+rm $packages_path/bazel_0.25.2.tar.gz.*
+rm $packages_path/v6.7.2.tar.gz.*
+rm $packages_path/v3.4.0.tar.gz.*
+rm $packages_path/mobilenet_v1_1.0_224.tgz.*
+rm $packages_path/ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18.tar.gz.*
+rm $packages_path/mklml_lnx_2019.0.5.20190502.tgz.*
---
+++ debian/split_packages.sh
@@ -0,0 +1,15 @@
+split -b 49m packages/0e906ebc527eab1cdbf7adabff5b474da9562e9f.tar.gz packages/0e906ebc527eab1cdbf7adabff5b474da9562e9f.tar.gz.
+split -b 49m packages/bazel_0.25.2.tar.gz packages/bazel_0.25.2.tar.gz.
+split -b 49m packages/v6.7.2.tar.gz packages/v6.7.2.tar.gz.
+split -b 49m packages/v3.4.0.tar.gz packages/v3.4.0.tar.gz.
+split -b 49m packages/mobilenet_v1_1.0_224.tgz packages/mobilenet_v1_1.0_224.tgz.
+split -b 49m packages/ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18.tar.gz packages/ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18.tar.gz.
+split -b 49m packages/mklml_lnx_2019.0.5.20190502.tgz packages/mklml_lnx_2019.0.5.20190502.tgz.
+
+rm packages/0e906ebc527eab1cdbf7adabff5b474da9562e9f.tar.gz
+rm packages/bazel_0.25.2.tar.gz
+rm packages/v6.7.2.tar.gz
+rm packages/v3.4.0.tar.gz
+rm packages/mobilenet_v1_1.0_224.tgz
+rm packages/ssd_mobilenet_v1_quantized_300x300_coco14_sync_2018_07_18.tar.gz
+rm packages/mklml_lnx_2019.0.5.20190502.tgz
